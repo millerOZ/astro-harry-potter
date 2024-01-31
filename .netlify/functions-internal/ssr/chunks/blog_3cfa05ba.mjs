96c22d284d64c9a9f1534fbe724cae7b62331d8f
Miller Ossa samboni <millerossasamboni@PSOFKA0192-2.local>
2024-01-30 23:23:17 -0500

    test deploy

--- .netlify/functions-internal/ssr/chunks/blog_3cfa05ba.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/blog_366a738a.mjs').then(n => n.e);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_119837b6.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_79dae3a8.mjs');
-
-export { page };
